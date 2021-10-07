7dde6462af879f6404f86a7e76f2b64dc6d54f5e
Abdurrahim <tolgabaskesen@hotmail.com>
2021-10-07 17:46:29 +0300

    language created

---
+++ scripts/lang.sh
@@ -0,0 +1,3 @@
+flutter pub run easy_localization:generate -S "assets/lang" -O "lib/init/generated"  
+
+flutter pub run easy_localization:generate -S "assets/lang" -O "lib/init/generated" -o "locale_keys.g.dart" -f keys 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:MTSK/main.dart';
+import 'package:mtsk/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
