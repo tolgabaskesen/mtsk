b84471b1e94bc560d1bb3d357fa696f18f046e06
Abdurrahim <tolgabaskesen@hotmail.com>
2021-10-07 17:46:22 +0300

    language created.

---
+++ lib/init/generated/locale_keys.g.dart
@@ -0,0 +1,41 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  // ignore: constant_identifier_names
+  static const language_screen_title = 'language_screen.title';
+  // ignore: constant_identifier_names
+  static const language_screen_cancel = 'language_screen.cancel';
+  // ignore: constant_identifier_names
+  static const language_screen = 'language_screen';
+  // ignore: constant_identifier_names
+  static const home_screen_scan_barcode_button_text = 'home_screen.scan_barcode_button_text';
+  // ignore: constant_identifier_names
+  static const home_screen_stop_video = 'home_screen.stop_video';
+  // ignore: constant_identifier_names
+  static const home_screen_write_barcode = 'home_screen.write_barcode';
+  // ignore: constant_identifier_names
+  static const home_screen_brands = 'home_screen.brands';
+  // ignore: constant_identifier_names
+  static const home_screen_info = 'home_screen.info';
+  // ignore: constant_identifier_names
+  static const home_screen_contact = 'home_screen.contact';
+  // ignore: constant_identifier_names
+  static const home_screen_web = 'home_screen.web';
+  // ignore: constant_identifier_names
+  static const home_screen = 'home_screen';
+  // ignore: constant_identifier_names
+  static const drawer_widget_language = 'drawer_widget.language';
+  // ignore: constant_identifier_names
+  static const drawer_widget_contact_us = 'drawer_widget.contact_us';
+  // ignore: constant_identifier_names
+  static const drawer_widget = 'drawer_widget';
+  // ignore: constant_identifier_names
+  static const video_player_cannot_play = 'video_player.cannot_play';
+  // ignore: constant_identifier_names
+  static const video_player_scan_cannot_play = 'video_player.scan_cannot_play';
+  // ignore: constant_identifier_names
+  static const video_player_ok = 'video_player.ok';
+  // ignore: constant_identifier_names
+  static const video_player = 'video_player';
+
+}
